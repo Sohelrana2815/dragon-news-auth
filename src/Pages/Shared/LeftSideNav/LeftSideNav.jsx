0f1bf61e1d86ae654fe3fa80fdc36d878ae10c18
Sohelrana2815 <sohel152302@gmail.com>
2024-05-29 10:26:42 +0600

    middle nav shared

---
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -0,0 +1,11 @@
+
+const LeftSideNav = () => {
+    return (
+        <div>
+            <h2 className="text-2x">Left side</h2>
+            
+        </div>
+    );
+};
+
+export default LeftSideNav;
---
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,10 @@
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2 className="text-2x">Right side</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
