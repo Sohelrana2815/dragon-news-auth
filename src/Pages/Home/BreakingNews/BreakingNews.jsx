6574c67bc163d0a3f5b78f4a33f6452f4ed32b85
Sohelrana2815 <sohel152302@gmail.com>
2024-05-29 16:04:42 +0600

    first

---
+++ src/Pages/Home/BreakingNews/BreakingNews.jsx
@@ -0,0 +1,43 @@
+import Marquee from "react-fast-marquee";
+
+const BreakingNews = () => {
+  return (
+    <div className="flex">
+      <button className="btn btn-secondary">Breaking News</button>
+      <Marquee className="cursor-pointer" pauseOnHover={true} speed={100}>
+        <span className="ml-12  text-gray-700">
+          UN Supports Relief Efforts After Cyclone Remal:
+          <a
+            className="btn btn-link"
+            target="_blank"
+            href="https://www.globalissues.org/news/2024/05"
+          >
+            Global issues
+          </a>
+        </span>
+        <span className="ml-12 text-gray-700">
+          Israeli Airstrikes in Rafah:
+          <a
+            className="btn btn-link"
+            target="_blank"
+            href="https://www.timesofisrael.com/"
+          >
+            The Time of Israel
+          </a>
+        </span>
+        <span className="ml-12 text-gray-700">
+          US Suspends Aid Deliveries to Gaza:
+          <a
+            className="btn btn-link"
+            target="_blank"
+            href="https://www.timesofisrael.com/"
+          >
+            The Time of Israel
+          </a>
+        </span>
+      </Marquee>
+    </div>
+  );
+};
+
+export default BreakingNews;
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,10 +1,13 @@
-
+import logo from "../../../assets/logo.png";
+import moment from "moment";
 const Header = () => {
-    return (
-        <div>
-             <h2 className="text-2xl">Header</h2>
-        </div>
-    );
+  return (
+    <div className="text-center">
+      <img className="mx-auto " src={logo} alt="" />
+      <p>Journalism Without Fear or Favour</p>
+      <p className="text-xl">{moment().format("dddd, MMMM D, YYYY")}</p>
+    </div>
+  );
 };
 
 export default Header;
